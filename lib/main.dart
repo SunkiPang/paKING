366bb5d72076767ec069dd3166449e94d185a7e0
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2021-06-18 16:39:02 +0900

    [ADD] 초기 페이지

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MainPage(),
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+      ),
+    );
+  }
+}
