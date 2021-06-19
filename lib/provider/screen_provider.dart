dbe777ddb7fb092f1d7515ee96354536556b17cf
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2021-06-19 18:40:31 +0900

    [UPDATE] 프로바이더에 맵스크린 필터 관련 추가 및 맵 라우팅 완료

--- lib/provider/screen_provider.dart
+++ lib/provider/screen_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class ScreenProvider with ChangeNotifier {
+  //전체 스크린 컨트롤
   int _selectedIndex = 0;
 
   int get screenIndex => _selectedIndex;
+
+  //Map 스크린 필터 값 및 페이지 조정
+  bool _isMap = true;
+
+  bool get isMap => _isMap;
 
   void changeScreen(int selected) {
     _selectedIndex = selected;
     notifyListeners();
   }
+
+  void changeMapFilter() {
+    _isMap ? _isMap = false : _isMap = true;
+    notifyListeners();
+  }
 }
+
--- lib/show_google_map.dart
+++ lib/show_google_map.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:test_flutter/filter_screen.dart';
+import 'package:test_flutter/provider/screen_provider.dart';
 
 class ShowGoogleMap extends StatefulWidget {
   Completer<GoogleMapController> _controller = Completer();
@@ -16,50 +19,59 @@ class ShowGoogleMap extends StatefulWidget {
 }
 
 class _ShowGoogleMapState extends State<ShowGoogleMap> {
+  List<bool> _filterValues = [true, true, true, true, true, true];
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        GoogleMap(
-          mapType: MapType.normal,
-          markers: Set<Marker>.of(widget.markers.values),
-          // YOUR MARKS IN MAP
-          initialCameraPosition: CameraPosition(
-            target: LatLng(36.103591549634345, 129.38800959286618),
-            zoom: 17,
-          ),
-          onMapCreated: (GoogleMapController controller) {
-            widget._controller.complete(controller);
-          },
-          myLocationButtonEnabled: true,
-        ),
-        Positioned(
-          top: 10,
-          right: 10,
-          child: Container(
-            padding: EdgeInsets.all(4),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              shape: BoxShape.circle,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    spreadRadius: 2,
-                    blurRadius: 7,
-                    offset: Offset(0, 2), // changes position of shadow
-                  ),
-                ],
-            ),
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.filter_alt,
-                color: Colors.white,
+    final screen = Provider.of<ScreenProvider>(context);
+    return screen.isMap
+        ? Stack(
+            children: [
+              GoogleMap(
+                mapType: MapType.normal,
+                markers: Set<Marker>.of(widget.markers.values),
+                // YOUR MARKS IN MAP
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(36.103591549634345, 129.38800959286618),
+                  zoom: 17,
+                ),
+                onMapCreated: (GoogleMapController controller) {
+                  widget._controller.complete(controller);
+                },
+                myLocationButtonEnabled: true,
               ),
-            ),
-          ),
-        )
-      ],
-    );
+              Positioned(
+                top: 10,
+                right: 10,
+                child: Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        spreadRadius: 2,
+                        blurRadius: 7,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        screen.changeMapFilter();
+                      });
+                    },
+                    child: Icon(
+                      Icons.filter_alt,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        : FilterScreen();
   }
 }
