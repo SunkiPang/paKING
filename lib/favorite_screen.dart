7f6de191b6a56dd7c6e3e7390cefc7ae89cbc96a
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2021-06-19 18:41:02 +0900

    [ADD] 즐겨찾기 제작 및 리스트뷰 적용 //아직 삭제기능 x

---
+++ lib/favorite_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteScreenState createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<String> favorite = <String>[
+      '한동대학교 히딩크필드',
+      '영일대 주차장',
+      '양덕동 주차장',
+      '양천구 주차장'
+    ];
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "즐겨찾기",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+            ),
+            SizedBox(
+              height: size.height * 0.04,
+            ),
+            Container(
+              width: size.width,
+              height: size.height,
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                itemCount: favorite.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              favorite[index],
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  favorite.remove(index);
+                                });
+                              },
+                              icon: Icon(Icons.delete),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: size.height * 0.01,
+                        ),
+                        Divider(),
+                        SizedBox(
+                          height: size.height * 0.01,
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
