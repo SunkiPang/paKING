68a6a65149b8772dcc76949f091a89a381fc94fa
SunkiPang <49421213+SunkiPang@users.noreply.github.com>
2021-06-21 16:50:51 +0900

    [UPDATE] 기기 정보 받아오는 것 추가

--- lib/mypage.dart
+++ lib/mypage.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
+import 'package:flutter/services.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({Key? key}) : super(key: key);
@@ -8,7 +12,58 @@ class MyPage extends StatefulWidget {
   _MyPageState createState() => _MyPageState();
 }
 
+Map<String, dynamic> _readAndroidBuildData(AndroidDeviceInfo build) {
+  return <String, dynamic>{
+    'version.securityPatch': build.version.securityPatch,
+    'version.sdkInt': build.version.sdkInt,
+    'version.release': build.version.release,
+    'version.previewSdkInt': build.version.previewSdkInt,
+    'version.incremental': build.version.incremental,
+    'version.codename': build.version.codename,
+    'version.baseOS': build.version.baseOS,
+    'board': build.board,
+    'bootloader': build.bootloader,
+    'brand': build.brand,
+    'device': build.device,
+    'display': build.display,
+    'fingerprint': build.fingerprint,
+    'hardware': build.hardware,
+    'host': build.host,
+    'id': build.id,
+    'manufacturer': build.manufacturer,
+    'model': build.model,
+    'product': build.product,
+    'supported32BitAbis': build.supported32BitAbis,
+    'supported64BitAbis': build.supported64BitAbis,
+    'supportedAbis': build.supportedAbis,
+    'tags': build.tags,
+    'type': build.type,
+    'isPhysicalDevice': build.isPhysicalDevice,
+    'androidId': build.androidId,
+    'systemFeatures': build.systemFeatures,
+  };
+}
+
+Map<String, dynamic> _readIosDeviceInfo(IosDeviceInfo data) {
+  return <String, dynamic>{
+    'name': data.name,
+    'systemName': data.systemName,
+    'systemVersion': data.systemVersion,
+    'model': data.model,
+    'localizedModel': data.localizedModel,
+    'identifierForVendor': data.identifierForVendor,
+    'isPhysicalDevice': data.isPhysicalDevice,
+    'utsname.sysname:': data.utsname.sysname,
+    'utsname.nodename:': data.utsname.nodename,
+    'utsname.release:': data.utsname.release,
+    'utsname.version:': data.utsname.version,
+    'utsname.machine:': data.utsname.machine,
+  };
+}
+
 class _MyPageState extends State<MyPage> {
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, dynamic> _deviceData = <String, dynamic>{};
   bool isModify = false;
   TextEditingController nameController = TextEditingController();
   TextEditingController carNumController = TextEditingController();
@@ -20,9 +75,36 @@ class _MyPageState extends State<MyPage> {
     nameController.text = "김한동";
     carNumController.text = '1';
     phoneNumController.text = 'xxxx-xxxx-xxxx';
+    initPlatformState();
 
     super.initState();
   }
+
+  Future<void> initPlatformState() async {
+    Map<String, dynamic> deviceData = <String, dynamic>{};
+
+    try {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, dynamic>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _deviceData = deviceData;
+    });
+  }
+
+  //여기까지는 기기정보 받는거
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -198,7 +280,11 @@ class _MyPageState extends State<MyPage> {
                               style: TextStyle(fontWeight: FontWeight.bold),
                             ),
                             Text(
-                              "xxxx-xxxx-xxxx",
+                              _deviceData.isEmpty
+                                  ? "..."
+                                  : Platform.isAndroid
+                                      ? _deviceData['androidId']
+                                      : _deviceData['identifierForVendor'],
                               style: TextStyle(color: Colors.grey),
                             )
                           ],
